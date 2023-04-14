620d24576afe4a70a32fee72ccf0a4d6a3e312c2
Felix Theilen <felix.theilen@tuhh.de>
2023-04-14 11:18:41 +0200

    Update manual_test_conversion_error.py

--- test/manual_test_conversion_error.py
+++ test/manual_test_conversion_error.py
@@ -1,24 +1,20 @@
 import sys
+
 sys.path.append("C:/Users/ftheilen/Source/ocelot")
 
 from cheetah import accelerator
-from ocelot import (Drift, Monitor)
+from ocelot import Drift, Monitor
 
-#Drift
+# Drift
 drift_arlisolg1 = Drift(l=0.19600000000000006, eid="Drift_ARLISOLG1")
 drift_arliscrg1 = Drift(l=0.19600000000000006, eid="Drift_ARLISCRG1")
 
-#Monitors
+# Monitors
 arlibscl1 = Monitor(eid="ARLIBSCL1")
 arshscre1 = Monitor(eid="ARSHSCRE1")
 
-#lattice
-cell = (
-    drift_arlisolg1,
-    drift_arliscrg1,
-    arlibscl1,
-    arshscre1
-)
+# lattice
+cell = (drift_arlisolg1, drift_arliscrg1, arlibscl1, arshscre1)
 
 segment = accelerator.Segment.from_ocelot(cell)
 
