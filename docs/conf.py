3684bbfade1ccaf0a2673713e9d7c1516db8a2ad
FelixTheilen <felix.theilen@tuhh.de>
2023-05-05 15:52:34 +0200

    Recreated the README document
    Recreated the README document in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@ release = '2022'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.highlighting'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
